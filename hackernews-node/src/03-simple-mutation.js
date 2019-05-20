5bdd57643fb56e4194c2d371124c7fc1ab4292de
Herryanto Siatono <hsiatono@paypal.com>
2019-05-20 15:22:18 +0800

    add mutation and schema examples

--- hackernews-node/src/03-simple-mutation.js
+++ hackernews-node/src/03-simple-mutation.js
@@ -1,22 +1,23 @@
 const { GraphQLServer } = require('graphql-yoga')
 
-let links = [
-  {
+let links = {
+  'link-0': {
     id: 'link-0',
     url: 'www.hotographql.com',
     description: 'Full stack tutorial for GraphQL',
   },
-]
+}
 
 let idCount = links.length
 
 const resolvers = {
   Query: {
     info: () => 'This is the API of a Hackernews Clone',
-    feed: () => links,
-    link: id => {
-      console.log('>> linnk ', id)
-      links.find(link => link.id === id)
+    feed: () => {
+      return Object.values(links)
+    },
+    link: (parent, args, context, info) => {
+      return links[args.id]
     },
   },
   Mutation: {
@@ -29,11 +30,26 @@ const resolvers = {
       links.push(link)
       return link
     },
+    updateLink: (parent, args) => {
+      const link = links[args.id]
+      if (link) {
+        Object.keys(args).forEach(key => {
+          link[key] = args[key]
+        })
+        return link
+      }
+      return null
+    },
+    deleteLink: (parent, args) => {
+      const link = links[args.id]
+      delete links[args.id]
+      return link
+    },
   },
 }
 
 const server = new GraphQLServer({
-  typeDefs: './src/03-schema.graphql',
+  typeDefs: './src/schema.graphql',
   resolvers,
 })
 
